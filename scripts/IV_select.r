fa8025deba5c5cece8597a9fe5087eb5e4bb2a43
Liu Junhui <103775557+LockenLiu@users.noreply.github.com>
2024-06-27 17:12:06 +0800

    Update IV_select.r
    Website of phonoscanner sometimes can not be accessed.

--- scripts/IV_select.r
+++ scripts/IV_select.r
@@ -40,28 +40,28 @@ confounders <- c("smoking","alcohol","education","college","university","smoke",
 exp_ma_data <- fread(file = exp_ma_file, header = T, data.table = F, select = c("SNP", "P"))
 exp_ma_data <- exp_ma_data[na.omit(match(data[,8],exp_ma_data$SNP)),] %>% unique()
 exp_ma_data <- exp_ma_data[exp_ma_data$P < 5e-8,]
-if(nrow(exp_ma_data) == 0){
-	confounder_snps=c()
-	print("Notice: No availuable IVs!")
-}else if(nrow(exp_ma_data) <= 100 & nrow(exp_ma_data) > 0){
-	scan_res <- phenoscanner(snpquery = exp_ma_data$SNP, pvalue = 5e-8, r2 = 0.8)$results
-	confounder_snps <- scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
-}else{
-	confounder_snps=c()
-	for(i in seq(floor(nrow(exp_ma_data)/100))){
-		scan_res <- phenoscanner(snpquery = exp_ma_data$SNP[(100*(i-1)+1):(100*i)], pvalue = 5e-8)$results
-		snps = scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
-		confounder_snps = c(confounder_snps, snps)
-	}
-	if(nrow(exp_ma_data)%%100 != 0){
-		scan_res <- phenoscanner(snpquery = exp_ma_data$SNP[(nrow(exp_ma_data)-(nrow(exp_ma_data)%%100)+1):nrow(exp_ma_data)], pvalue = 5e-8)$results
-		snps = scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
-		confounder_snps = c(confounder_snps, snps)
-	}
-}
-if(length(confounder_snps) != 0){
-	exp_ma_data <- exp_ma_data[-which(exp_ma_data$SNP %in% confounder_snps),]
-}
+#if(nrow(exp_ma_data) == 0){
+#	confounder_snps=c()
+#	print("Notice: No availuable IVs!")
+#}else if(nrow(exp_ma_data) <= 100 & nrow(exp_ma_data) > 0){
+#	scan_res <- phenoscanner(snpquery = exp_ma_data$SNP, pvalue = 5e-8, r2 = 0.8)$results
+#	confounder_snps <- scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
+#}else{
+#	confounder_snps=c()
+#	for(i in seq(floor(nrow(exp_ma_data)/100))){
+#		scan_res <- phenoscanner(snpquery = exp_ma_data$SNP[(100*(i-1)+1):(100*i)], pvalue = 5e-8)$results
+#		snps = scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
+#		confounder_snps = c(confounder_snps, snps)
+#	}
+#	if(nrow(exp_ma_data)%%100 != 0){
+#		scan_res <- phenoscanner(snpquery = exp_ma_data$SNP[(nrow(exp_ma_data)-(nrow(exp_ma_data)%%100)+1):nrow(exp_ma_data)], pvalue = 5e-8)$results
+#		snps = scan_res[grep(paste(confounders,collapse="|"),tolower(scan_res$trait)),"rsid"] %>% unique()
+#		confounder_snps = c(confounder_snps, snps)
+#	}
+#}
+#if(length(confounder_snps) != 0){
+#	exp_ma_data <- exp_ma_data[-which(exp_ma_data$SNP %in% confounder_snps),]
+#}
 data2 <- merge(exp_ma_data, data, by.x="SNP", by.y="hit_gwas")
 for (cell in strsplit(cells, split=",")[[1]]){
 	if (! cell %in% data2$cell){
